c63c7f66e88611737ad9e6396b6f915e3dbbe905
Trisha Boda <141979558+trishaboda@users.noreply.github.com>
2024-06-20 13:18:17 +0530

    app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftContainerComponent } from './left-container/left-container.component';
+import { RightContainerComponent } from './right-container/right-container.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftContainerComponent,
+    RightContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [
     provideClientHydration()
