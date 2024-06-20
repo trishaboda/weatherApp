f4d8f512148826d0e71e4cf3bdfb5a083b07b822
Trisha Boda <141979558+trishaboda@users.noreply.github.com>
2024-06-20 13:17:59 +0530

    left container

---
+++ src/app/left-container/left-container.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { faCloudRain, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
+import { faCloud } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-left-container',
+  templateUrl: './left-container.component.html',
+  styleUrl: './left-container.component.css'
+})
+export class LeftContainerComponent {
+  faMagnifyingGlass:any = faMagnifyingGlass;
+  faLocation:any = faLocation;
+  faCloud:any = faCloud;
+  faCloudRain:any = faCloudRain;
+}
