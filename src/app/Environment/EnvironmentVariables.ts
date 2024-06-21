aecee7663b6d0537bde794760f3bc7c151fef58b
Trisha Boda <141979558+trishaboda@users.noreply.github.com>
2024-06-21 17:19:01 +0530

    file for env variables/ api

---
+++ src/app/Environment/EnvironmentVariables.ts
@@ -0,0 +1,14 @@
+export const EnvironmentVariables = {
+    production: false,
+    // base uRLs to access the end points using the http client get method
+    weatherApiLocationBaseURL: 'https://weather338.p.rapidapi.com/locations/search?',
+    weatherApiForecastBaseURL: 'https://weather338.p.rapidapi.com/weather/forecast?',
+
+    //variables for API key name and value
+    xRapidApiKeyName: 'x-rapidapi-key',
+    xRapidApiKeyValue: '3b742eb441msh711c6af28b196b8p1df1e7jsn1395a8cfa60b',
+
+    //variables for host name and value
+    xRapidApiHostName: 'x-rapidapi-host',
+    xRapidApiHostValue: 'weather338.p.rapidapi.com',
+}
