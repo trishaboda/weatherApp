15df01f376e91a54077ef9dd9e4448ac501b6473
Trisha Boda <141979558+trishaboda@users.noreply.github.com>
2024-06-21 12:20:38 +0530

    fa icon ts update

--- src/app/right-container/right-container.component.ts
+++ src/app/right-container/right-container.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import { faFaceSmile } from '@fortawesome/free-solid-svg-icons';
+import { faFaceFrown } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-right-container',
   templateUrl: './right-container.component.html',
   styleUrl: './right-container.component.css'
 })
 export class RightContainerComponent {
+
+  //fa icons
+  faThumbsUp:any = faThumbsUp;
+  faThumbsDown:any = faThumbsDown;
+  faFaceSmile:any = faFaceSmile;
+  faFaceFrown:any = faFaceFrown;
+
   //control tabs variables
   today: boolean = false;
   week: boolean = true;
